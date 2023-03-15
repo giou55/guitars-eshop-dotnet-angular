65f169752ad259bc47a20c2b06ef0d457f8cf46c
giou55 <giourmetakis00@gmail.com>
2023-03-15 19:21:31 +0200

    changes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
@@ -11,15 +12,16 @@ const routes: Routes = [
   {path: 'test-error', component: TestErrorComponent},
   {path: 'not-found', component: NotFoundComponent},
   {path: 'server-error', component: ServerErrorComponent},
+  {path: 'contact', component: ContactComponent},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule)},
   {path: 'basket', loadChildren: () => import('./basket/basket.module').then(m => m.BasketModule)},
   {
-    path: 'checkout', 
+    path: 'checkout',
     canActivate: [AuthGuard],
     loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule)
   },
   {
-    path: 'orders', 
+    path: 'orders',
     canActivate: [AuthGuard],
     loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule)
   },
